0e9016782523592a55b1701d92fa5eeacdd44aee
Johan Lundgren <lundgren.johan99@gmail.com>
2022-11-08 20:55:29 +0100

    add(Card): Added grid

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,28 @@
+import styled from "styled-components";
+
 function Card() {
+    const GridContainer = styled.div`
+        width: 90vw;
+        text-align: center;
+        background-color: #7722CC;
+        margin: auto;
+    `;
+
+    const VoteButton = styled.button`
+        height: 500px;
+        width: 500px;
+        border-radius: 10px;
+        font-size: 15pt;
+    `;
+
     const id = "test";
 
     return(
-        <div className="card">
-            <button className="card-button">test</button>
-        </div>
+        <GridContainer>
+            <div className="card">
+                <VoteButton className="card-button">test</VoteButton>
+            </div>
+        </GridContainer>
     )
 }
 
