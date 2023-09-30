bd77eb044b95673c9ac1ce4c7e7e748adaa81007
Johan <lundgren.johan99@gmail.com>
2023-09-30 21:40:01 +0200

    feat(NavbarScroller): created testsuite for navbar

---
+++ src/components/NavbarScroller.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NavbarScroller from './NavbarScroller';
+
+const navigation = {
+    brand: { name: "Voter", to: "/" },
+    links: [
+      { name: "About Me", to: "/about" },
+      { name: "Blog", to: "/blog" },
+      { name: "Developement", to: "/dev" },
+      { name: "Graphic Design", to: "/design" },
+      { name: "Contact", to: "/contact" },
+    ]
+  }
+
+test('renders Navbar', () => {
+    const { brand, links } = navigation;
+
+    render(<NavbarScroller brand={brand} links={links} />);
+    
+    const brandElement = screen.getByText(/Voter/i);
+    expect(brandElement).toBeInTheDocument();
+
+    const linkElement = screen.getByText(/About Me/i);
+    expect(linkElement).toBeInTheDocument();
+});
