7f75c09f461c5b337df6af69f80c640dd6240d65
Johan <lundgren.johan99@gmail.com>
2023-09-30 21:05:09 +0200

    feat: initial navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'reset-css';
+import NavbarScroller from './components/NavbarScroller';
 
 function App() {
+  const navigation = {
+    brand: { name: "Voter", to: "/" },
+    links: [
+      { name: "About Me", to: "/about" },
+      { name: "Blog", to: "/blog" },
+      { name: "Developement", to: "/dev" },
+      { name: "Graphic Design", to: "/design" },
+      { name: "Contact", to: "/contact" },
+    ]
+  }
+  const { brand, links } = navigation;
+
   return (
     <div className="App">
+      <NavbarScroller brand={brand} links={links} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
